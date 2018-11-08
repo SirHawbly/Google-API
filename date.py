405c2aca71c2fb4ccbce10b2933d43c9a79d4596
sirhawbly <cbar@VideoAdmins-MacBook-Air.local>
2018-11-07 22:38:02 -0800

    finished the time delta function

--- date.py
+++ date.py
@@ -1,6 +1,11 @@
 import datetime as d
-from datetime import date
 
 todays = d.datetime(day=7,month=9,year=2018, hour=14, minute=30)
 
-print(d.datetime.today() - todays)
+def get_remain_days(hr, mn, dy, mnth, yr):
+	event_day = d.datetime(hour=hr, minute=mn, day=dy, month=mnth, year=yr)
+	return event_day - d.datetime.today()
+
+diff = get_remain_days(14,30,13,8,2019)
+
+print(diff.days)
